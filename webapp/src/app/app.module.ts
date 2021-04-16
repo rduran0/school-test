62c33bded3d27ffbce59301668a5f531b235fc23
andresjz94 <ljuarez@iwa.com.mx>
2021-04-16 01:29:26 -0500

    WEEK-5
    componentes 1

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Main routes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
